86822db2a7a2f38d3e408cf3a49ffac0926e7c38
Patricia Antlitz <patriciaantlitz@Patricias-MacBook-Pro.local>
2020-01-07 19:38:06 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,14 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipper " + cardThree);
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+const cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again")
+} 
